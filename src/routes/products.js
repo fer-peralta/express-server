6bddad5aadf8052421488f67e5e25e944ee1be96
fer-peralta <fernandoemanuelperalta@gmail.com>
2023-02-02 21:03:56 -0300

    updated products Router

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,37 +2,45 @@ import express from "express"
 import { checkAdminRole } from "../middlewares/checkRole.js"
 import {ContenedorDaoProductos} from "../daos/index.js"
 import { checkLogin } from "../middlewares/checkLogin.js"
+import { logger } from "../loggers/loggers.js"
 
 const productosApi = ContenedorDaoProductos
 
 const productsRouter = express.Router();
 
-productsRouter.get('/', async (req, res) => {
+productsRouter.get('/', checkLogin, async (req, res) => {
     const response = await productosApi.getAll()
-    res.json(response)
+    res.status(200).json(response)
 })
 
 productsRouter.get('/:id', checkLogin, async (req, res) => {
-    const productId = parseInt(req.params.id);
+    // const productId = parseInt(req.params.id); // * Only with fyleSystem
+    const productId = req.params.id
     const response = await productosApi.getById(productId);
-    res.json(response);
+    res.status(200).json(response);
 })
 
-productsRouter.post('/', checkAdminRole, async (req, res) => {
+productsRouter.post('/', checkLogin, checkAdminRole, async (req, res) => {
     const response = await productosApi.save(req.body);
-    res.json(response)
+    logger.info("Se agregó un producto de la base de datos")
+    res.status(200).json({response: response, message: "El producto fue agregado a la base de datos"})
 })
 
-productsRouter.put('/:id', checkAdminRole, async (req, res) => {
-    const productId = parseInt(req.params.id);
-    const response = await productosApi.updateById(req.body, productId);
-    res.json(response);
+productsRouter.put('/:id', checkLogin, checkAdminRole, async (req, res) => {
+    // const productId = parseInt(req.params.id); // * Only with fileSystem
+    const productId = req.params.id
+    // const response = await productosApi.updateById(req.body, productId); // * Only with fileSystem
+    const response = await productosApi.putById(productId, req.body);
+    logger.info("Se actualizó un producto de la base de datos")
+    res.status(200).json({response: response, message: "El producto seleccionado fue actualizado"});
 })
 
-productsRouter.delete('/:id', checkAdminRole, async (req, res) => {
-    const productId = parseInt(req.params.id);
+productsRouter.delete('/:id', checkLogin, checkAdminRole, async (req, res) => {
+    // const productId = parseInt(req.params.id); // * Only with fileSystem
+    const productId = req.params.id
     const response = await productosApi.deleteById(productId);
-    res.json(response);
+    logger.info("Se eliminó un producto de la base de datos")
+    res.status(200).json({response: response, message: "El producto seleccionado fue eliminado"});
 })
 
 export {productsRouter}
